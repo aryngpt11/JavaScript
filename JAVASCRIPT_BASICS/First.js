c0292d564795b2d2e9118611fcb8cef1833ab53d
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-13 20:41:21 +0530

    js

---
+++ JAVASCRIPT_BASICS/First.js
@@ -0,0 +1,12 @@
+//console.log("Aryan Gupta");
+/* fname="AryanGupta"
+console.log(fname); */
+
+const student={
+    fname:"Aryan Gupta",
+    age:22,
+    cgpa:8.1,
+    isPass:true
+} 
+console.log(student);
+console.log(student["fname"])
