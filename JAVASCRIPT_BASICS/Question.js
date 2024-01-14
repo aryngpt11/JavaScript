cbd4c4c1390bcf2031856f27935651747d96d3c0
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-14 21:10:51 +0530

    question

---
+++ JAVASCRIPT_BASICS/Question.js
@@ -0,0 +1,20 @@
+const product={
+    title:" Ball Pen",
+    rating:4,
+    offer:5,
+    price:270
+};
+console.log(product);
+console.log(product["offer"])
+console.log(typeof product);
+
+
+//ques2
+
+const profile={
+    uname:"aryngpt11",
+    isfollow:false,
+    followers:561,
+    following:131
+};
+console.log(profile);
