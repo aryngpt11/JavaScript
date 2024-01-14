82cc579432a847c52d471f4b59bba6397e78de49
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-14 21:31:30 +0530

    divibility

--- JAVASCRIPT_BASICS/First.js
+++ JAVASCRIPT_BASICS/First.js
@@ -2,11 +2,21 @@
 /* fname="AryanGupta"
 console.log(fname); */
 
-const student={
+/* const student={
     fname:"Aryan Gupta",
     age:22,
     cgpa:8.1,
     isPass:true
 } 
 console.log(student);
-console.log(student["fname"])
+console.log(student["fname"]) */
+/* let name=prompt("Hello");
+console.log(name); */
+
+let a=prompt("Enter number to check the divisibility by 5");
+if (a/5==0){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
