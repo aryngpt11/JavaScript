07c5c7702b4092edd6eff8d24ddcd9a96285aa56
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-15 23:39:19 +0530

    game

---
+++ JAVASCRIPT_BASICS/Ques4game.js
@@ -0,0 +1,9 @@
+let gameNum=25;
+let userNum=prompt("Guessthe number");
+//console.log(userNum);
+
+while(userNum !=gameNum){
+    userNum=prompt("u entered wrong number ,guess again");
+
+}
+console.log("congratulation,u entered the right number")
