091605555f6fac0629bf8dc800844df0a2dfd9dc
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-15 23:23:56 +0530

    ques3

---
+++ JAVASCRIPT_BASICS/Question3.js
@@ -0,0 +1,13 @@
+console.log("print even number")
+let a=0;
+/* while(a<=100){
+    console.log(a);
+    a++;
+} */
+while(a<=100){
+    //if (a%2!=0) odd number
+    if(a%2==0){
+        console.log(a);
+    }
+    a++;
+}
