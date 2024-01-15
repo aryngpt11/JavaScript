735ed3307406d3bdc218ef85754905fbc6eb20e0
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-15 23:11:25 +0530

    loops

--- JAVASCRIPT_BASICS/loopss.js
+++ JAVASCRIPT_BASICS/loopss.js
@@ -8,12 +8,24 @@ for(let i=0;i<=5;i++){
 };
 console.log(summ); */
 
-//for of loop
+//for of loop used for string and
 
-let str="Aryan";
+/* let str="Aryan";
 let size=0;
 for(let i of str){
     console.log(i)
     size++;
 }
-console.log(size);
+console.log(size); */
+
+//for in loop used for objects
+
+let student={
+    name:"Aryan Gupta",
+    age:22,
+    cgpa:8.1,
+    isPass:true
+};
+for(let i in student){
+    console.log(i);
+}
