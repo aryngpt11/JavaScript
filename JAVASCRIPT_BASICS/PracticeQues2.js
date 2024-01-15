9877d9c76265bdf83d504893aeaeadae333485db
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-15 23:00:01 +0530

    js

---
+++ JAVASCRIPT_BASICS/PracticeQues2.js
@@ -0,0 +1,18 @@
+let score=75;
+let grade;
+
+if(score >=90 && score <=100){
+    grade="A";
+}else if(score >=70 && score<=89){
+    grade="B";
+}
+else if(score >=60 && score<=69){
+    grade="C";
+}
+else if(score >=50 && score<=59){
+    grade="C";
+}
+else{
+    grade="F"
+}
+console.log(grade);
---
+++ JAVASCRIPT_BASICS/loopss.js
@@ -0,0 +1,19 @@
+/* for(let i=0;i<5;i++){
+    console.log("Aryan");
+} */
+
+/* let summ=0;
+for(let i=0;i<=5;i++){
+    summ+=i;
+};
+console.log(summ); */
+
+//for of loop
+
+let str="Aryan";
+let size=0;
+for(let i of str){
+    console.log(i)
+    size++;
+}
+console.log(size);
