1c09fdf566d2040d19f50519d310b3f0b17145f8
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-16 14:07:50 +0530

    template literals

---
+++ JAVASCRIPT_BASICS/StringgLiteral.js
@@ -0,0 +1,11 @@
+/* let specialString=`This is a template literal`;
+console.log(specialString);
+console.log(typeof specialString); */
+
+let obj={
+    item:"pen",
+    price:10
+};
+//console.log(obj["item"]);
+console.log("this is a",obj.item,"of ",obj.price,"ruppess")// using string
+console.log(`this is a ${obj.item} of ${obj.price} ruppess`)// using tempplate literals
