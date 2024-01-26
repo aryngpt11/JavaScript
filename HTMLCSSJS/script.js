8fe84ea61240828c0cd7d4306faac051c4aaf70e
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-26 18:01:25 +0530

    Practiceques

--- HTMLCSSJS/script.js
+++ HTMLCSSJS/script.js
@@ -20,4 +20,26 @@ console.dir(elements);
 let elementsall=document.querySelectorAll(".heading");
 console.dir(elementsall); */
 
-console.dir(document.body.firstChild);
+//console.dir(document.body.firstChild);
+
+//let div=document.querySelector("div");
+//console.dir(div);
+
+//let heading=document.querySelector("h1");
+
+//practice question
+
+/* let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText=h2.innerText+" I am Aryan"; */
+
+let divs =document.querySelectorAll(".box");
+//console.log(divs[0]);
+let idx=1;
+for(div of divs){
+    div.innerText="new unique value"+idx;
+    idx++
+}
+/* divs[0].innerText="This is unique text1"
+divs[1].innerText="This is unique text2s"
+divs[2].innerText="This is unique text3" */
