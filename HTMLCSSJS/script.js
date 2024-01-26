7e7d55fca29ff91bda5888eb824c101159e7fa0a
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-01-26 17:22:10 +0530

    dom

--- HTMLCSSJS/script.js
+++ HTMLCSSJS/script.js
@@ -1,2 +1,23 @@
-console.log("Hello")
-alert("Aryan Gupta")
+/* console.log("Hello")
+alert("Aryan Gupta") */
+/* console.dir(document.body);
+console.log(document.body); */
+/* let header=document.getElementById("heading")
+console.dir(header); */
+
+
+/* let headings=document.getElementsByClassName("heading");
+console.dir(headings);
+console.log(headings); */
+
+/* let parg=document.getElementsByTagName("p");
+console.dir(parg); */
+
+/* 
+let elements=document.querySelector(".heading");
+console.dir(elements);
+
+let elementsall=document.querySelectorAll(".heading");
+console.dir(elementsall); */
+
+console.dir(document.body.firstChild);
