3b110828728e12542bf4138a15bfbdd4c4c4499f
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-02-12 16:18:58 +0530

    js

--- HTMLCSSJS/script1.js
+++ HTMLCSSJS/script1.js
@@ -1,4 +1,16 @@
-let div=document.querySelector("div");
+/* let div=document.querySelector("div");
 console.log(div);
 let id=div.getAttribute("id");
 console.log(id); 
+let name=div.getAttribute("name")
+console.log(name) */
+
+let para=document.querySelector("p")
+//console.log(para.getAttribute("class"));
+console.log(para.setAttribute("class","newclass"))
+
+let div=document.querySelector("div");
+div.style.backgroundColor="green";
+div.style.fontSize="22px"
+div.innerText="Hello"
+console.log(div)
