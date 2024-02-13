daae1e91837d61307fbec24dcfa03623af2ee610
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2024-02-13 23:10:57 +0530

    Dom

--- HTMLCSSJS/script1.js
+++ HTMLCSSJS/script1.js
@@ -5,7 +5,7 @@ console.log(id);
 let name=div.getAttribute("name")
 console.log(name) */
 
-let para=document.querySelector("p")
+/* let para=document.querySelector("p")
 //console.log(para.getAttribute("class"));
 console.log(para.setAttribute("class","newclass"))
 
@@ -13,4 +13,18 @@ let div=document.querySelector("div");
 div.style.backgroundColor="green";
 div.style.fontSize="22px"
 div.innerText="Hello"
-console.log(div)
+console.log(div) */
+
+let btn=document.createElement("button");
+btn.innerText="click Me";
+console.log(btn);
+let div=document.querySelector("div");
+div.append(btn);
+
+let newHeading=document.createElement("h1");
+newHeading.innerHTML="<i>Hi,I am new!</i>";
+document.querySelector("body").prepend(newHeading);
+let para=document.querySelector("p");
+para.remove();
+
+newHeading.remove();
